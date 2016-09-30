1094ebfbc5ac100f9a6085b119de8a4afa17e27f
AmmarNqvi <ammarnaqvi92@gmail.com>
2016-09-29 20:23:34 -0700

    Implementing pow(x, n) using recursion

---
+++ pow(x,n).cpp
@@ -0,0 +1,12 @@
+//Implementing pow(x, n) using recursion
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+       if (n == 0) return 1;
+       if (n == 2) return x * x;
+       if (n % 2 == 0) return myPow(myPow(x, n / 2), 2);
+       else if (n > 0) return x * myPow(myPow(x, n / 2), 2);
+       else return 1 / x * myPow(myPow(x, n / 2), 2);
+    }
+};
